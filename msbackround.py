67754d974164a70329288d7db143db52c9e5fcd5
Jeremy Fleischman <jeremyfleischman@gmail.com>
2012-02-18 10:57:37 -0800

    shit werks bitches

--- msbackround.py
+++ msbackround.py
@@ -1,5 +1,6 @@
 import subprocess
 import hackthon
+import time
 
 networks={"AirBears.Berkeley"}
 
@@ -8,7 +9,13 @@ def check_for_networks():
     process = subprocess.Popen(["ipconfig"],stdout= subprocess.PIPE)
     stdout,stderr = process.communicate()
     for network in networks:
+        print("Checking if we're on %s" % network)
         if network in stdout:
             hackthon.connect()
 
+
+if __name__ == "__main__":
+	while 1:
+		check_for_networks()
+		time.sleep(5)
 
