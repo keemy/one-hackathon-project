ad66253fb10e95466dcb0799206f2c09b0c8e787
Jeremy Fleischman <jeremyfleischman@gmail.com>
2012-02-18 06:39:47 -0800

    Added some graceful handling of data.txt.

--- hackthon.py
+++ hackthon.py
@@ -2,6 +2,8 @@ import http.client
 import urllib.request, urllib.parse
 import http.cookiejar
 
+import sys
+
 
 headers = { "Content-type": "application/x-www-form-urlencoded","Accept": "text/plain" }
 url = "/cas/login?service=https%3a%2f%2fwlan.berkeley.edu%2fcgi-bin%2flogin%2fcalnet.cgi%3fsubmit%3dCalNet%26url%3d"
@@ -23,19 +25,25 @@ hidden = shit[start:end]
 
 #print(hidden,"\n\n\n\n\n")
 #import pdb; pdb.set_trace()
-f=open("data.txt")
+try:
+	f = open("data.txt")
+	un_pas = str(f.read()).split("\n")
+	assert len(un_pas) == 2
+	username, password = un_pas
+	assert username
+	assert password
+except:
+	print("Could not open file data.txt, please create data.txt with username and password separated by one newline")
+	sys.exit(1)
 
-un_pas=str(f.read()).split("\n")
-assert len(un_pas)==2
-
-values = { "username": un_pas[0], "password": un_pas[1], "lt":hidden, "_eventId":"submit" }
+values = { "username": username, "password": password, "lt":hidden, "_eventId":"submit" }
 
 data = urllib.parse.urlencode(values)
 
 conn = http.client.HTTPSConnection("auth.berkeley.edu")
 conn.request("POST", url, data, headers)
 response = conn.getresponse()
-if response.status!=302:
+if response.status != 302:
     if "The CalNet ID and/or Passphrase you provided are incorrect." in str(response.read()):
         raise Exception("rong username/password")
     else:
